5c63a1595ad219dff6e44e26eff531254e97a4f6
vantheman2210 <martinovic.ba@gmail.com>
2023-05-08 13:40:01 +0200

    Adding validation before form submission

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 import styles from "./AddUser.module.css";
 
 const AddUser = () => {
   const [user, setUser] = useState({
-    name: "",
+    username: "",
     age: "",
   });
 
@@ -20,33 +21,43 @@ const AddUser = () => {
 
   const onSubmitHandler = (e) => {
     e.preventDefault();
-    e.target.reset();
+    if (user.username.trim() === 0 || user.age.trim().length === 0) {
+      return;
+    }
+    if (user.age < 0) return;
+
+    setUser({
+      username: "",
+      age: "",
+    });
     console.log(user);
   };
 
   return (
-    <Card>
-    <form onSubmit={onSubmitHandler} className="form-control">
-      <label htmlFor="username">
-        Username:
-        <input
-          type="text"
-          id="username"
-          name="username"
-          onChange={onChangeHandler}
-        ></input>
-      </label>
-      <label htmlFor="age">
-        Age:
-        <input
-          type="text"
-          id="age"
-          name="age"
-          onChange={onChangeHandler}
-        ></input>
-      </label>
-      <button type="submit">Add user</button>
-    </form>
+    <Card className={styles.input}>
+      <form onSubmit={onSubmitHandler} className="form-control">
+        <label htmlFor="username">
+          Username:
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={user.username}
+            onChange={onChangeHandler}
+          ></input>
+        </label>
+        <label htmlFor="age">
+          Age:
+          <input
+            type="text"
+            id="age"
+            name="age"
+            value={user.age}
+            onChange={onChangeHandler}
+          ></input>
+        </label>
+        <Button type="submit">Add user</Button>
+      </form>
     </Card>
   );
 };
