7dd95e091f1cac4c47be09617df854887184afd9
vantheman2210 <martinovic.ba@gmail.com>
2023-05-08 11:40:14 +0200

    Creating adduser component

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,8 +1,24 @@
-import React from "react";
-import styles from './AddUser.module.css';
+import React, { useState } from "react";
+import styles from "./AddUser.module.css";
 
-const AddUser = () => { 
+const AddUser = () => {
+  const [user, setUser] = useState({
+    name: "",
+    age: "",
+  });
 
+  return (
+    <form>
+      <label>
+        Name:
+        <input></input>
+      </label>
+      <label>
+        Age:
+        <input></input>
+      </label>
+    </form>
+  );
 };
 
 export default AddUser;
